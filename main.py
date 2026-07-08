0448f450c7401727838a7e958646d394741839c8
Reyhan Nur Tanrıtanır <134210477+reyhanmills@users.noreply.github.com>
2026-07-08 22:08:00 +0300

    Read text documents from documents folder

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-def main():
-    print("Local RAG Assistant project started.")
+from pathlib import Path
+
+#main fonksiyonu tanımlıyorum
+#belgelerin konumunu belirliyorum ve .txt ile biten tum belgeleri alıyoeuz.
+#turkçe karakterler bozulmasın diye utf-8
+def main(): 
+    documents_folder = Path("documents")
+    for file_path in documents_folder.glob("*.txt"):
+        content = file_path.read_text(encoding="utf-8")
+        print("Dosya adı:", file_path.name)
+        print("Dosya içeriği:")
+        print(content)
 
 
 if __name__ == "__main__":
     main()
+
+    #bu dosya çalışırsa main fonksiyonunu çalıştır.
